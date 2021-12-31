f1c74f04413c97627a681cdf8eb24e95c4388fc5
anil kumar <anilk@nimapinfotech.com>
2021-12-31 13:00:06 +0530

    app's endpoints got structured

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,30 +4,30 @@ from myapp import views
 urlpatterns = [
     
     #function based views endpoints =>
-    # path("", views.products, name='products'),
-    # path("<int:pk>/", views.products_detail, name='products_detail'),
+    # path("api/products/", views.products, name='products'),
+    # path("api/products/<int:pk>/", views.products_detail, name='products_detail'),
 
     #class based views endpoints =>
-    # path("", views.CategoryListView.as_view(), name="category"),
-    # path("create/", views.CategoryCreateView.as_view(), name="create_category"),
-    # path("category/<int:pk>/", views.CategioryDetailView.as_view(), name="get_category_id"),
-    # path("update/<int:pk>/", views.CategoryUpdateView.as_view(), name="update_category"),
-    # path("delete/<int:pk>/", views.CategoryDeleteView.as_view(), name="delete_category")
+    # path("api/categories/", views.CategoryListView.as_view(), name="category"),
+    # path("api/categories/post/", views.CategoryCreateView.as_view(), name="create_category"),
+    # path("api/categories/category/<int:pk>/", views.CategioryDetailView.as_view(), name="get_category_id"),
+    # path("api/categories/update/<int:pk>/", views.CategoryUpdateView.as_view(), name="update_category"),
+    # path("api/categories/delete/<int:pk>/", views.CategoryDeleteView.as_view(), name="delete_category")
 
     #generic views endpoints
-    # path("", views.ProductListView.as_view(), name="list_product"),
-    # path("create_product/", views.ProductCreateView.as_view(), name="create_product"),
-    # path("get_product/<int:pk>/", views.ProductRetrieveView.as_view(), name="get_product"),
-    # path("update_product/<int:pk>/", views.ProductUpdateView.as_view(), name="update_product"),
-    # path("delete_product/<int:pk>/", views.ProductDeleteView.as_view(), name="delete_product"),
+    # path("api/products/", views.ProductListView.as_view(), name="list_product"),
+    # path("api/products/post/", views.ProductCreateView.as_view(), name="create_product"),
+    # path("api/products/get/<int:pk>/", views.ProductRetrieveView.as_view(), name="get_product"),
+    # path("api/products/update/<int:pk>/", views.ProductUpdateView.as_view(), name="update_product"),
+    # path("api/products/delete/<int:pk>/", views.ProductDeleteView.as_view(), name="delete_product"),
 
     #nested serializer endpoints
-    path("category_api/", views.CategoryListCreateView.as_view(), name="category_api"),
-    path("category_api/<int:pk>/", views.CategoryRUDView.as_view(), name="category_rud_api"),
-    path("product_api/", views.ProductListCreateView.as_view(), name="product_api"),
-    path("product_api/<int:pk>/", views.ProductRUDView.as_view(), name="product_rud_api"),
-    path("faq_api/", views.FaqListCreateView.as_view()),
-    path("faq_api/<int:pk>/", views.FaqRUDView.as_view())
+    path("api/categories/", views.CategoryListCreateView.as_view(), name="category_api"),
+    path("api/categories/<int:pk>/", views.CategoryRUDView.as_view(), name="category_rud_api"),
+    path("api/products/", views.ProductListCreateView.as_view(), name="product_api"),
+    path("api/products/<int:pk>/", views.ProductRUDView.as_view(), name="product_rud_api"),
+    path("api/faq/", views.FaqListCreateView.as_view()),
+    path("api/faq/<int:pk>/", views.FaqRUDView.as_view())
     
 ]
 
